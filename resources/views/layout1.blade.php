aff180ee413d62b5839b39470fab8ca3b9284969
Bruce Shiundu <bruce.shiundu@nokia.com>
2022-12-22 10:34:50 +0100

    Tried fixing some css issues

--- resources/views/layout1.blade.php
+++ resources/views/layout1.blade.php
@@ -12,11 +12,11 @@
     <title>Detec truth - Home</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
     @yield('scripts')
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+        <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('/css/style.css')}}">
 
     <!-- Custom styles for this template-->
--- resources/views/my-reports.blade.php
+++ resources/views/my-reports.blade.php
@@ -18,7 +18,11 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">My reports</h1>
+                        <a href="{{ route('crime.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                        class="fas fa-download fa-sm text-white-50"></i> Report Crime</a>
+                    </div>
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">My reports</h1>
